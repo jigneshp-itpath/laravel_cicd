036621c6316be13d48b5d3d5304dc0481add6ddd
Jignesh Prajapati <jigneshp@itpathsolutions.com>
2024-06-12 19:08:56 +0530

    site update

--- deploy.sh
+++ deploy.sh
@@ -4,32 +4,35 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+php artisan down
 
-    cp server.php index.php
+echo "copy files start ..."
+cp server.php index.php
+echo "copy files end ..."
 
-    # Update codebase
-    git fetch origin main
-    git reset --hard origin/main
+# Update codebase
+git fetch origin main
+git reset --hard origin/main
 
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+# Install dependencies based on lock file
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
-    # Migrate database
-    # php artisan migrate --force
+# Migrate database
+# php artisan migrate --force
 
-    # Note: If you're using queue workers, this is the place to restart them.
-    # ...
+# Note: If you're using queue workers, this is the place to restart them.
+# ...
 
-    # permission set
-    chmod 777 -R storage/
-    chmod 777 -R bootstrap/
+# Permission given
+chmod 777 -R storage/
+chmod 777 -R bootstrap/
 
-    # Clear cache
-    php artisan optimize
+# Clear cache
+php artisan optimize
 
-    # Reload PHP to update opcache
-    # echo "" | sudo -S service php7.4-fpm reload    
+# Reload PHP to update opcache
+# echo "" | sudo -S service php7.4-fpm reload    
+
 # Exit maintenance mode
 php artisan up
 
